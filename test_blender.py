0702541752998cba6bed4c89913d7f2bc46d1e21
Eric McNeel <ericmcneel@gmail.com>
2026-03-27 19:46:26 +0100

    Initial venv setup

---
+++ test_blender.py
@@ -0,0 +1,7 @@
+import bpy  # The secret library we just talked about!
+
+# Let's ask Blender to tell us something about itself
+print("\n" + "="*30)
+print("Hello from inside Blender Version:", bpy.app.version_string)
+print("="*30 + "\n")
+
